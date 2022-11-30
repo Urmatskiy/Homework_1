f689488f1361fd55ea42b938f624085e2b6e5e28
Urmatskiy <nickiton14@gmail.com>
2022-11-30 15:35:57 +0300

    Сделал первое задание

---
+++ task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine());
+int max = first;
+if (second > max)
+{
+    max = second;    
+}
+Console.WriteLine($"Наибольшим числом является {max}");
